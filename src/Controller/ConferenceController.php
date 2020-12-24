f4d20fabd982acaa3ec57cebf4d5edea98593d0f
Jose Cuenca <jcuenca@monsol.net>
2020-12-24 13:04:09 +0100

    paso 10 . Interfaz de usuario

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +15,34 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return $this->render('conference/index.html.twig');
+        $conferences = $conferenceRepository->findAll();
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferences
+        ]);
     }
+
+    // nueva pagina. 
+    // Otra opcion es crear un controlador nuevo para cada pagina
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, CommentRepository $commentRepository, Conference $conference)
+    {
+        // symfony busca la conferencia, y aquise buscan los comentarios
+
+        $offset= max(0, $request->query->getInt('offset', 0));;
+
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+        ]);
+
+    }
+
 }
